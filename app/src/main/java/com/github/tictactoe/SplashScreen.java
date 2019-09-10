c6e8d23ce3883415e1dade438d9af5d5c7495535
samdherron <samdherron@gmail.com>
2019-09-10 16:08:04 -0400

    Initial Commit

---
+++ app/src/main/java/com/github/tictactoe/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.github.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SplashScreen extends AppCompatActivity
+        implements View.OnClickListener {
+
+    Button btnStartGame;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        btnStartGame = findViewById(R.id.btnStartGame);
+        btnStartGame.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent gameScreen = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(gameScreen);
+    }
+}
